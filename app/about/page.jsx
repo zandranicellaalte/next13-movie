233eab34c19e54786c350ba84e335341367bbee1
Zandra Nicella Alte <85550797+zandranicellaalte@users.noreply.github.com>
2023-01-30 20:07:33 +0100

    Revert "initial commit"
    This reverts commit fcefd9a76ed065584d0daf3d2c8efdb71f1725cf.

--- app/about/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  // throw new Error('failed to load data');
-  return (
-    <main>
-      <h1> About </h1>
-    </main>
-  );
-}
--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    domains: ['image.tmdb.org'],
-  },
-};
+}
 
 module.exports = nextConfig
